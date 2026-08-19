5586828b53abbc5f7150d3ddc8416b6dcb74fa4c
viliusbartainis <vilius.bartainis67@gmail.com>
2026-08-19 20:54:03 +0300

    Add Google Fonts preconnect links in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,12 @@ export default async function RootLayout({
   return (
     <html lang={locale} className="h-full antialiased">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,500;0,9..144,600;1,9..144,500&family=Work+Sans:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
