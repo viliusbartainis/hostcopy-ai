34bfbf108108a92fb1951c4fc522c17572c6942a
viliusbartainis <vilius.bartainis67@gmail.com>
2026-08-28 15:49:08 +0300

    Add FontLoader component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import CookieConsent from "@/components/CookieConsent";
 import Analytics from "@/components/Analytics";
+import FontLoader from "@/components/FontLoader";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -51,9 +52,11 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
+          rel="preload"
+          as="style"
           href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Inter:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500&display=swap"
-          rel="stylesheet"
         />
+        <FontLoader />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
